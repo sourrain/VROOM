033e5674df3d868abcecd6cf0d947e618be8e1dc
Isabel Sun <sourrainnn@gmail.com>
2021-08-27 16:16:57 +0800

    Try

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,37 +1,9 @@
 <template>
-  <div>
-    <BlogPost />
-    <BlogPost />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import BlogPost from "../components/BlogPost.vue";
-export default {
-  components: { BlogPost },
-  data() {
-    return {
-      welcome: {
-        title: "Welcome!",
-        blogPost: "Welcome to VROOM notebook gallery.",
-        welcome: true,
-        photo: "sky",
-      },
-      sampleBlogPost: [
-        {
-          title: "This is sample notebook",
-          blogHTML: "This is notebook",
-          blogCoverPhoto: "designed-for-everyone",
-        },
-        {
-          title: "This is sample notebook",
-          blogHTML: "This is notebook",
-          blogCoverPhoto: "designed-for-everyone",
-        },
-      ],
-    };
-  },
-};
+export default {};
 </script>
 
 <style>
